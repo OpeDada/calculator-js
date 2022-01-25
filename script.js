06a5a732fa88dfd660c7561dbc5c63e1e4d9bb91
OpeDada <opedada@gmail.com>
2022-01-25 13:46:48 +0900

    clear previous operand

--- script.js
+++ script.js
@@ -115,6 +115,8 @@ class Calculator {
       this.previousOperandTextElement.innerText = `${this.getDisplayNumber(
         this.previousOperand
       )} ${this.operation}`;
+    } else {
+      this.previousOperandTextElement.innerText = "";
     }
   }
 }
